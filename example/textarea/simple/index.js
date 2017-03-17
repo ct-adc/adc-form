a0e588ef858b74387fe6aebd196b6f05b3cea832
skysunshine99 <skysunshine99@126.com>
2017-03-17 13:22:51 +0800

    first commit

---
+++ example/textarea/simple/index.js
@@ -0,0 +1,23 @@
+/**
+ * @author rubyisapm
+ */
+import Vue from 'vue';
+import Textarea from '../../../src/textarea/textarea.vue';
+new Vue({
+    el:'#app',
+    components:{
+        'ct-adc-textarea':Textarea
+    },
+    data:{
+        limitedLength:200,
+        cut:false
+    },
+    methods:{
+        changePass(pass){
+            console.log(pass)
+        },
+        change(content){
+            console.log(content);
+        }
+    }
+});
---
+++ src/textarea/textarea.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <textarea v-model="content"></textarea>
+
+        </div>
+        <div class="panel-footer">
+            <span :class="{'text-danger':content.length>limitedLength,'text-success':content.length<=limitedLength}">{{content.length}}</span>/{{limitedLength}}
+        </div>
+    </div>
+</template>
+
+<script type="es6">
+    export default{
+        name: 'ct-adc-textarea',
+        props: {
+            value: {
+                type: String,
+                default: ''
+            },
+            limitedLength: {
+                type: Number,
+                default: 500
+            },
+            cut: {
+                type: Boolean,
+                default: false
+            }
+        },
+        data(){
+            return {
+                content: ''
+            }
+        },
+        created(){
+            this.initContent();
+        },
+        computed:{
+            pass(){
+                return this.content.length <= this.limitedLength;
+            }
+        },
+        methods: {
+            initContent(){
+                this.content = this.value;
+            },
+            getContent(){
+                return this.content;
+            },
+            isPass(){
+                return this.pass;
+            }
+        },
+        watch: {
+            value(newVal){
+                this.content = newVal;
+            },
+            content(newVal, oldVal){
+                var cut = false;
+                if (newVal.length > this.limitedLength) {
+                    if (this.cut) {
+                        cut = true;
+                    }
+                }
+                if (cut) {
+                    this.content = newVal.substr(0, this.limitedLength);
+                    //don't emit event in this circle. it will emit in the next watch
+                } else {
+                    if (newVal !== oldVal) {
+                        this.$emit('change', newVal);
+                    }
+                }
+            },
+            pass(newVal,oldVal){
+                if(newVal!==oldVal){
+                    this.$emit('change-pass', newVal);
+                }
+            }
+        }
+    }
+</script>
+<style scoped>
+    textarea {
+        width: 100%;
+        outline: none;
+        border-color: transparent;
+        resize: none;
+        height: 200px;
+    }
+</style>
